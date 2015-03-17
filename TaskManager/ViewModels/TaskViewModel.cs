06418a0be59da9218a3db06db2f0b281b7780eed
blackzky <gervelgiva@gmail.com>
2015-03-17 17:23:54 +0800

    Added 'Priority to Image' conversion

--- TaskManager/ViewModels/TaskViewModel.cs
+++ TaskManager/ViewModels/TaskViewModel.cs
@@ -203,4 +203,24 @@ namespace TaskManager
 
 
     }
+
+    public class PriorityToImageConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return string.Format("/Resources/Images/Priority-{0}.png", value);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
